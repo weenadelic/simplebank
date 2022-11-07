69c96a4dcdd58730da4d072143ad1dd77cf527b3
JT <sc-boss@streetclosure.com>
2022-11-07 00:04:10 -0600

    Adding envriorment file using viper

--- main.go
+++ main.go
@@ -7,23 +7,22 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/weenadelic/simplebank/api"
 	db "github.com/weenadelic/simplebank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/weenadelic/simplebank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config")
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to postgres")
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server: ", err)
 	}
