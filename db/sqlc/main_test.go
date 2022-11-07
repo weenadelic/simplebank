fb228bb35d992ca7ce40e7e1a733745a86ba7a94
JT <sc-boss@streetclosure.com>
2022-11-07 00:08:29 -0600

    Fix tests to use viper env

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,12 +15,12 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 
-	config, err := util.LoadConfig(".")
+	config, err := util.LoadConfig("../..")
 	if err != nil {
 		log.Fatal("Cannot load config")
 	}
 
-	testDB, err := sql.Open(config.DBDriver, config.DBSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to postgres")
 	}
